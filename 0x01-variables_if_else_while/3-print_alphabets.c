e8915a8d756c68599d49b27d9859e84afd413268
Unrighteousboy <jacobgodwin281@gmail.com>
2023-12-17 00:29:03 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print a lowercase and uppercase alphabet
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
