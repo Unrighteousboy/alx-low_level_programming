6e14e4094cdd73aa8f5d52437ae1364ab1b76da8
Unrighteousboy <jacobgodwin281@gmail.com>
2023-12-17 03:21:56 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - print all single base 10 numbers
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a < 0; a++)
+	{
+		printf(a);
+	}
+	printf('\n');
+	return (0);
+}
