25cfa97fb254ae620d24501b0cceb9a4f7556845
Unrighteousboy <jacobgodwin281@gmail.com>
2023-12-27 05:52:03 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,17 @@
  */
 int main(void)
 {
-	char i;
+	int i;
+	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
-		putchar(', ');
+		for (j = 0; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(', ');
+		}
 	}
 	putchar('\n');
 	return(0);
