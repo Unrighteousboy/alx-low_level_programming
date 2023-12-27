38394efa39bb5deaea2cae5978d197c56a91fc0b
Unrighteousboy <jacobgodwin281@gmail.com>
2023-12-27 10:55:49 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print possible numbers of 10's and 1's
+ *
+ * Return: 0 success
+ *
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens < 10; tens++)
+	{
+		for (ones = 0; ones < 10; ones++)
+		{
+			if (!((ones == tens || tens < ones)))
+			{
+				putchar(tens);
+				putchar(ones);
+			}
+			if (!(ones == 9) && (tens == 8))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
