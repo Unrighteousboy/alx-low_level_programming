50aaccdf79f5b5e48626809bf7feb307ee9acc63
Unrighteousboy <jacobgodwin281@gmail.com>
2024-01-01 08:38:03 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,16 @@
 /**
  * main - print lowercase alphabet
  *
- * Return: 0 success
+ * Return: void
  *
  */
 void print_alphabet(void)
 {
-	int i;
+	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 		_putchar('\n');
 	}
-	return (0);
 }
