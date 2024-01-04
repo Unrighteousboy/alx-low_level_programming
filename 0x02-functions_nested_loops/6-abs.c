5105889cc12b8e0c66d68ce51037147888dd1038
Unrighteousboy <jacobgodwin281@gmail.com>
2024-01-04 05:05:42 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of a numver
+ * Return: the absolute value
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int abs = int * -1;
+		return (abs);
+	}
+	return (int);
+}
