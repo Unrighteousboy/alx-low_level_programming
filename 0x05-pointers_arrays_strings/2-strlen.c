843594ce80fdb41aeb4153bef4f6e069e12d4dd9
Unrighteousboy <jacobgodwin281@gmail.com>
2024-01-07 11:38:09 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - count the length of array
+ * @s: character to be checked
+ * Return: the length of array
+ */
+int _strlen(char *s)
+{
+	int i, count;
+	
+	count = 0;
+	
+	for (i = 0; s[i] != '/0'; i++)
+	{
+		count++;
+		return (count);
+	}
+}
