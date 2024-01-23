32090a57b2fffa56c4114df1ce9bb44b668d5378
Unrighteousboy <jacobgodwin281@gmail.com>
2024-01-23 09:08:16 -0800

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - print attributes of dog
+ * @d: type of struct dog
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s", (*d).name? (*d).name : "(nil)");
+		printf("Age: %.1f", (*d).age? (*d).age : 0);
+		printf("Owner: %s", (*d).owner? (*d).owner : "(nil)");
+	}
+}
