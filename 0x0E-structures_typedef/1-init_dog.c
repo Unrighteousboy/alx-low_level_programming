612db44c6cbb3757cacd6a446df5eb6b2965c24c
Unrighteousboy <jacobgodwin281@gmail.com>
2024-01-23 08:27:39 -0800

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initialize values to type struct dog
+ * @d: struct dog
+ * @name: name parameter
+ * @age: age parameter
+ * @owner: owner parameter
+ * Return: Void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
