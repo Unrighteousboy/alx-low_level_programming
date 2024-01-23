2184e93d61d3a1d29e1b18766c1bba3f1fc43431
Unrighteousboy <jacobgodwin281@gmail.com>
2024-01-23 08:04:48 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_HEADER_FILE
-#define DOG_HEADER_FILE
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * dog - Create a struct
  * Return: 0
@@ -9,4 +9,10 @@ struct dog {
 	float age;
 	char *owner;
 }
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
