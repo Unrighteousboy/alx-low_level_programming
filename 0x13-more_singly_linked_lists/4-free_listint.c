c2582542b2d4f189af1b570fff1173a9b5e7352b
Unrighteousboy <jacobgodwin281@gmail.com>
2024-02-05 13:46:25 -0800

    3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - free a list
+ * @head: head of a node
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->n);
+		free(head);
+		head = temp;
+	}
+}
