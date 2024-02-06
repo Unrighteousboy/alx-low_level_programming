db9ceb292cd847bb9a35df96e8259ff159e97d9e
Unrighteousboy <jacobgodwin281@gmail.com>
2024-02-06 13:15:48 -0800

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,12 +8,19 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
 	temp = head;
 
 	if (!head)
 		return (NULL);
+
+	if (idx == 0)
+	{
+		new->next = head;
+		head = new;
+		return (new);
+	}
 
 	for (; i < index; i++)
 	{
