e0ac4ad1619d609074d2abfaa5ea01a1273d5651
Unrighteousboy <jacobgodwin281@gmail.com>
2024-02-06 08:15:37 -0800

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a list
+ * @head: the head node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
